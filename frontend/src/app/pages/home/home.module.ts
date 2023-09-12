373c422d3742fe9d9597595b8de54c71140a2c45
larboulletmarin <andrea.larboullet-marin@avisto.com>
2023-09-12 08:57:39 +0200

    :sparkles: feat(home): add new home components

--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { APP_CONSTANTS } from 'src/app/app.constant';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 
 const homeRoutes: Routes = [
   { path: APP_CONSTANTS.routerLinks.home, component: HomePageComponent },
@@ -14,7 +16,7 @@ const MODULES = [CommonModule, SharedModule, RouterModule.forChild(homeRoutes)];
 const COMPONENTS = [HomePageComponent];
 
 @NgModule({
-  declarations: [COMPONENTS],
+  declarations: [COMPONENTS, CarouselComponent, PlaceholderComponent],
   imports: [MODULES],
   exports: [RouterModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
