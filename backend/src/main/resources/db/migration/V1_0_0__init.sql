387e697fca0185f92a0166c668a15703038a8388
larboulletmarin <andrea.larboullet-marin@avisto.com>
2023-09-12 08:58:47 +0200

    :card_file_box: modify init sql script

--- backend/src/main/resources/db/migration/V1_0_0__init.sql
+++ backend/src/main/resources/db/migration/V1_0_0__init.sql
@@ -26,9 +26,46 @@ CREATE TABLE product
 
 CREATE INDEX idx_product_category ON product (category_id);
 
-
--- CATEGORY
+-- ADD CONSTANT DATA CATEGORY
 INSERT INTO category (name, code)
 VALUES ('Electronics', 'ELECTRONICS');
 INSERT INTO category (name, code)
 VALUES ('Books', 'BOOKS');
+
+-- ADD DEVELOPMENT DATA PRODUCT
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1234', 'iPhone 5s', 'Apple iPhone 5s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera', 500, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1235', 'Dell Inspiron', 'Dell Inspiron 14-inch Laptop (Black) with 3rd Generation Intel Core processors', 700, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (2, 'P1236', 'Samsung Galaxy Tab 4', 'Samsung Galaxy Tab 4 T231 Tablet (7-inch, 8GB, WiFi, 3G, Voice Calling), Black', 400, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (2, 'P1237', 'One Plus One', 'OnePlus One (Sandstone Black, 64GB)', 350, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (2, 'P1238', 'Nexus 9', 'HTC Google Nexus 9 (8.9-inch, 16GB, WiFi, LTE, Voice Calling), Indigo Black', 450, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (2, 'P1239', 'Lenovo Yoga', 'Lenovo Yoga 8 Tablet (8-inch, 16GB, WiFi, 3G, Voice Calling), Silver', 300, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1240', 'HP Notebook', 'HP 15-r063tu Notebook (4th Gen Ci3/ 4GB/ 500GB/ Free DOS) (K8T73PA)', 550, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1241', 'Lenovo Thinkpad', 'Lenovo Thinkpad X1 Carbon 20A80056IG Laptop (4th Gen Ci5/ 4GB/ 256GB/ Win8.1)', 750, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1242', 'Dell Inspiron', 'Dell Inspiron 15 3542 Notebook (4th Gen Ci5/ 4GB/ 1TB/ Win8.1/ 2GB Graph)', 650, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1243', 'Apple MacBook', 'Apple MacBook Pro MD101HN/A 13-inch Laptop (Core i5/4GB/500GB/Mac OS Mavericks/Intel HD Graphics)', 850, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1244', 'HP Envy', 'HP Envy 15-k006tx Notebook (4th Gen Ci5/ 8GB/ 1TB/ Win8.1/ 2GB Graph/ Touch)', 950, 'assets/images/products/placeholder.png', TRUE, 100);
+
+INSERT INTO product (category_id, code, name, description, unit_price, image_url, active, units_in_stock)
+VALUES (1, 'P1245', 'Dell Inspiron', 'Dell Inspiron 15 3542 Notebook (4th Gen Ci7/ 8GB/ 1TB/ Win8.1/ 2GB Graph)', 1050, 'assets/images/products/placeholder.png', TRUE, 100);
+
