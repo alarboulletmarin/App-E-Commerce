55bb958c44359eb95becc19f2f0e179f50df0d41
alarboulletmarin <andrea.larboullet-marin@outlook.com>
2023-10-09 12:30:21 +0200

    :sparkles: feat: add register form @ToDo

--- frontend/src/app/pages/auth/register/register.module.ts
+++ frontend/src/app/pages/auth/register/register.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { FormComponent } from './components/form/form.component';
 
 const homeRoutes: Routes = [{ path: '', component: RegisterPageComponent }];
 
@@ -11,7 +12,7 @@ const MODULES = [CommonModule, SharedModule, RouterModule.forChild(homeRoutes)];
 const COMPONENTS = [RegisterPageComponent];
 
 @NgModule({
-  declarations: [COMPONENTS],
+  declarations: [COMPONENTS, FormComponent],
   imports: [MODULES],
   exports: [RouterModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
