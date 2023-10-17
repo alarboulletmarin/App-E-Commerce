125033c8dbd236cbd141b6195a82abf52e20fea0
alarboulletmarin <andrea.larboullet-marin@outlook.com>
2023-10-17 12:48:46 +0200

    route restrictions and methods

--- backend/src/main/java/com/formation/backend/config/security/WebSecurityConfig.java
+++ backend/src/main/java/com/formation/backend/config/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.formation.backend.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,15 +40,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors().and()
                 .authorizeRequests(requests -> requests
+                        // Endpoints publics
                         .antMatchers("/api/auth/signin", "/api/auth/register", "/api/auth/logout", "/v3/api-docs",
                                 "/configuration/ui",
                                 "/swagger-resources/**",
                                 "/configuration/security",
                                 "/swagger-ui/**",
                                 "/webjars/**").permitAll()
+                        .antMatchers(HttpMethod.GET, "/api/products/**", "/api/categories/**").permitAll()
+
+                        // Restrict access to the admin and user APIs
                         .antMatchers("/api/admin/**").hasRole("ADMIN")
                         .antMatchers("/api/users/**").hasRole("USER")
-                        .antMatchers("/api/products/**", "/api/categories/**").permitAll()
+                        .antMatchers(HttpMethod.POST, "/api/products/**", "/api/categories/**").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.PUT, "/api/products/**", "/api/categories/**").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/api/products/**", "/api/categories/**").hasRole("ADMIN")
+
+                        // all other requests need to be authenticated
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable());
     }
